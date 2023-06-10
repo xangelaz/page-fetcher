5ad5ff4ecc94759af06ff9f401d27b1e6176a356
Angela Zhou <a03zhou@gmail.com>
2023-06-10 01:25:28 -0400

    fetcher complete

---
+++ fetcher.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const fs = require('fs');
+let url = process.argv[2];
+let filePath = process.argv[3];
+
+request('https://www.example.edu', (error, response, body) => {
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('Downloaded and saved ' + body.length + ' bytes to ' + 'filePath')
+  fs.writeFile('/Users/angela/page-fetcher/index.html', body, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });  
+});
+
+
+// const content = body;
+
+// fs.writeFile('/Users/angela/page-fetcher/index.html', content, err => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   // file written successfully
+// });
+
